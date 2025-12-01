ddde753773573ded6f0718dc24480f02e6cde233
cloud-rooted <croninaj036@gmail.com>
2025-12-01 10:25:28 +0530

    Create s3-inorder-traversal.c

---
+++ s3-inorder-traversal.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *left, *right;
+};
+
+struct node* newNode(int x) {
+    struct node* n = malloc(sizeof(struct node));
+    n->data = x;
+    n->left = n->right = NULL;
+    return n;
+}
+
+void inorder(struct node* root) {
+    if(root == NULL) return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main() {
+    struct node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    inorder(root);
+    return 0;
+}
+
